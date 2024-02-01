6b1e3963cd73f4db60c6a138b226f8d01e23dba5
dalimkumar604 <dalimkumar212582@gmail.com>
2024-02-01 10:53:48 +0600

    asdfaslk

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import MainComponent from "./components/MaiComponent/MainComponent";
 
 import Icon1 from "./images/Icon1";
@@ -6,10 +7,12 @@ import Icon3 from "./images/Icon3";
 import Icon4 from "./images/Icon4";
 
 function App() {
-  const allData = {
+  const [allData, setAllData] = useState({
     fontFamily: "'Signika', sans-serif",
-    primaryColor: "#000",
-    secondaryColor: "#fff",
+    bgColor: "#fff",
+    textColor: "#000",
+    numbersColor: "#000",
+
     subHeading: "4 Tips 4",
     heading: "Brand Identity",
     data: [
@@ -17,34 +20,39 @@ function App() {
         icon: <Icon1 color="#8A6AD8" />,
         title: "Identify Yourself with A Face",
         info: `There are many companies in today's market that have "maskots" who act as icons for there overall brand image`,
-        bg: "#8A6AD8",
+        lineBg: "#8A6AD8",
+        shadowColor: "rgba(0, 0, 0, 0.15)",
       },
       {
         icon: <Icon2 color="#FF9772" />,
         title: "Being Authenic is important",
         info: `There are many companies in today's market that have "maskots" who act as icons for there overall brand image`,
 
-        bg: "#FF9772",
+        lineBg: "#FF9772",
+        shadowColor: "rgba(0, 0, 0, 0.15)",
       },
       {
         icon: <Icon3 color="#8A6AD8" />,
         title: "Provide seamless service",
         info: `There are many companies in today's market that have "maskots" who act as icons for there overall brand image`,
-        bg: "#8A6AD8",
+        lineBg: "#8A6AD8",
+        shadowColor: "rgba(0, 0, 0, 0.15)",
       },
       {
         icon: <Icon4 color="#FF9772" />,
         title: "You should be different",
         info: `There are many companies in today's market that have "maskots" who act as icons for there overall brand image`,
-        bg: "#FF9772",
+        lineBg: "#FF9772",
+        shadowColor: "rgba(0, 0, 0, 0.15)",
       },
     ],
-  };
+  });
   return (
     <div
       style={{
-        "--primaryColor": allData.primaryColor,
-        "--secondaryColor": allData.secondaryColor,
+        "--bgColor": allData.bgColor,
+        "--textColor": allData.textColor,
+        "--numbersColor": allData.numbersColor,
         fontFamily: allData.fontFamily,
       }}
     >
--- src/components/MaiComponent/MainComponent.js
+++ src/components/MaiComponent/MainComponent.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./MainComponent.module.css";
 const MainComponent = ({ subHeading, heading, data }) => {
+  const titleRefs = useRef([]);
+  useEffect(() => {
+    // Get the maximum height among all title elements
+    const titles = titleRefs.current.map(
+      (ref) => ref.getBoundingClientRect().height
+    );
+    const maxTitleHeight = Math.max(...titles);
+
+    // Set the maximum height to all title elements using the titleRefs
+    titleRefs.current.forEach((ref) => {
+      ref.style.height = `${maxTitleHeight}px`;
+    });
+  }, [data]);
   return (
     <div className={styles.mainWrapper}>
       <div className={styles.wrapper}>
@@ -13,7 +26,7 @@ const MainComponent = ({ subHeading, heading, data }) => {
         <div className={styles.cards}>
           {data.map((el, i) => (
             <div
-              style={{ "--bg": el.bg }}
+              style={{ "--lineBg": el.lineBg, "--shadowColor": el.shadowColor }}
               key={i}
               className={`${styles.card} ${styles[`items-${data.length}`]}  ${
                 data.length > 5 && styles[`items-5`]
@@ -26,7 +39,12 @@ const MainComponent = ({ subHeading, heading, data }) => {
                   <div className={styles.icon}>{el.icon}</div>
                 </div>
               </div>
-              <h4 className={styles.title}>{el.title}</h4>
+              <h4
+                className={styles.title}
+                ref={(el) => (titleRefs.current[i] = el)}
+              >
+                {el.title}
+              </h4>
               <p className={styles.info}>{el.info}</p>
             </div>
           ))}
